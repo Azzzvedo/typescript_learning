d911f00c83ff08a5c5aa61ee25614925280dc1ea
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-06-20 18:53:18 +0100

    core types reviewed

--- app.ts
+++ app.ts
@@ -1,9 +1,16 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+function add(n1: number, n2: number, printResult: boolean, resultPhrase: string) {
+  const result = n2 + n2;
+  if (printResult) {
+    console.log(resultPhrase + result);
+  } else {
+    return result;
+  }
 }
 
-const number1 = "5";
+const number1 = 5;
 const number2 = 2.8;
 
-const result = add(number1, number2);
-console.log(result);
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+const result = add(number1, number2, printResult, resultPhrase);
