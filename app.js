e09009028fdf3a56f4d1e02d2f5fa06a8a085ac1
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-06-21 10:08:04 +0100

    working with tuples

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var person = {
     name: "Maximilian",
     age: 30,
-    hobbies: ["Sports", "Cooking"]
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "author"]
 };
 var favoriteActivities;
 favoriteActivities = ["Sports"];
-console.log(person.name);
+// console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
 }
+person.role.push("admin");
+console.log(person.role);
--- app.ts
+++ app.ts
@@ -1,14 +1,25 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Maximilian",
   age: 30,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
 
-console.log(person.name);
+// console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
 }
+
+// tuple exception
+// person.role.push("admin");
+
+console.log(person.role);
