01df7302627d567d3128f1cacb7f982d5a37e295
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-06-21 11:28:46 +0100

    eworking with void

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-// combine function that favours type coercion to number rather than to string
-// function takes optional third param to force string or number conversion
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === "number" || typeof input2 === "number" || resultConversion === "as-number")
-        result = +input1 + +input2;
-    if (resultConversion === "as-text")
-        result = input1.toString() + input2.toString();
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedNums = combine(10, 15);
-console.log(combinedNums);
-var combinedAges = combine(30, 25, "as-number");
-console.log(combinedAges);
-var combinedNames = combine("Ian", "Rebecca", "as-text");
-console.log(combinedNames);
-var combinedAges2 = combine(24, "23");
-console.log(combinedAges2);
+function printResult(num) {
+    console.log("Result: " + num);
+}
+printResult(add(5, 12));
--- app.ts
+++ app.ts
@@ -1,24 +1,9 @@
-type Combinable = number | string;
-type ConversionDescriptor = "as-text" | "as-number";
-
-// combine function that favours type coercion to number rather than to string
-// function takes optional third param to force string or number conversion
-
-function combine(input1: Combinable, input2: Combinable, resultConversion?: ConversionDescriptor) {
-  let result;
-  if (typeof input1 === "number" || typeof input2 === "number" || resultConversion === "as-number") result = +input1 + +input2;
-  if (resultConversion === "as-text") result = input1.toString() + input2.toString();
-  return result;
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combinedNums = combine(10, 15);
-console.log(combinedNums);
+function printResult(num: number) {
+  console.log("Result: " + num);
+}
 
-const combinedAges = combine(30, 25, "as-number");
-console.log(combinedAges);
-
-const combinedNames = combine("Ian", "Rebecca", "as-text");
-console.log(combinedNames);
-
-const combinedAges2 = combine(24, "23");
-console.log(combinedAges2);
+printResult(add(5, 12));
