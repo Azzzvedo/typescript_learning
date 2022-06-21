d9bbc957bdfaa1b80399b937e51cfe3e728ae0c3
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-06-21 18:18:58 +0100

    working with never

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ userInput = "Max";
 if (typeof userInput === "string") {
     username = userInput;
 }
+var generateError = function (message, code) {
+    throw { message: message, errorCode: code };
+};
+generateError("An error occurred", 500);
--- app.ts
+++ app.ts
@@ -7,3 +7,9 @@ userInput = "Max";
 if (typeof userInput === "string") {
   username = userInput;
 }
+
+const generateError = function (message: string, code: number) {
+  throw { message: message, errorCode: code };
+};
+
+generateError("An error occurred", 500);
