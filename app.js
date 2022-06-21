2992ae06584c163495b2b61e4c42d949d0f624f5
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-06-21 10:15:18 +0100

    working with enums

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Maximilian",
     age: 30,
     hobbies: ["Sports", "Cooking"],
-    role: [2, "author"]
+    role: Role.ADMIN
 };
 var favoriteActivities;
 favoriteActivities = ["Sports"];
 // console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+    // console.log(hobby.toUpperCase());
 }
-person.role.push("admin");
-console.log(person.role);
+// tuple exception
+// person.role.push("admin");
+// console.log(person.role);
+if (person.role === Role.ADMIN) {
+    console.log("admin");
+}
--- app.ts
+++ app.ts
@@ -1,13 +1,14 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+enum Role {
+  ADMIN = 5,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "Maximilian",
   age: 30,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 let favoriteActivities: string[];
@@ -16,10 +17,13 @@ favoriteActivities = ["Sports"];
 // console.log(person.name);
 
 for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
+  // console.log(hobby.toUpperCase());
 }
 
 // tuple exception
 // person.role.push("admin");
 
-console.log(person.role);
+// console.log(person.role);
+if (person.role === Role.ADMIN) {
+  console.log("admin");
+}
