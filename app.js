86bf3628a42d5e0d4181e28c8becbf3301c7fba0
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-06-21 20:04:30 +0100

    some configuration

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-var userInput;
-var username;
-userInput = 5;
-userInput = "Max";
-if (typeof userInput === "string") {
-    username = userInput;
-}
-var generateError = function (message, code) {
-    throw { message: message, errorCode: code };
-};
-generateError("An error occurred", 500);
+"use strict";
+const userName = "Ian";
+console.log(userName);
---
+++ app.ts
@@ -0,0 +1,3 @@
+const userName = "Ian";
+
+console.log(userName);
