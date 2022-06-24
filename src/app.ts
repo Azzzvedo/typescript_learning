e810cbbaff01fcadb09454eec2de174e4b520499
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-06-24 15:06:59 +0100

    interfaces and classes

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,13 @@
-class Department {
+interface Person {
   name: string;
-  private employees: string[] = [];
-
-  constructor(n: string) {
-    this.name = n;
-  }
-
-  describe(this: Department) {
-    console.log(`Department: ${this.name}`);
-  }
-
-  addEmployee(this: Department, employee: string) {
-    this.employees.push(employee);
-  }
-
-  printEmployeeInformation() {
-    console.log(this.employees.length);
-    console.log(this.employees);
-  }
+  age: number;
+  greet(phrase: string): void;
 }
-const accounting = new Department("Accounting");
 
-accounting.addEmployee("Max");
-accounting.addEmployee("Manu");
-accounting.addEmployee("Anna");
-
-accounting.describe();
-accounting.printEmployeeInformation();
+const ian: Person = {
+  name: "ian",
+  age: 33,
+  greet(phrase) {
+    console.log(phrase);
+  },
+};
