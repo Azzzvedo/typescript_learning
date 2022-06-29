46f962cfda5f0989efe99f18eb1b1c5be042545f
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-06-29 14:47:44 +0100

    working with generics

--- src/app.ts
+++ src/app.ts
@@ -1,95 +1,41 @@
-type Admin = {
-  name: string;
-  privileges: string[];
-};
-
-type Employee = {
-  name: string;
-  startDate: Date;
-};
-
-type ElevatedEmployee = Admin & Employee;
-
-const e1: ElevatedEmployee = {
-  name: "Ian",
-  privileges: ["create-server"],
-  startDate: new Date(),
-};
-
-type Combinable = string | number;
-type Numeric = number | boolean;
-
-type Universal = Combinable & Numeric;
-
-function add(a: number, b: number): number;
-function add(a: string, b: string): string;
-function add(a: Combinable, b: Combinable) {
-  if (typeof a === "string" || typeof b === "string") {
-    return a.toString() + b.toString();
-  }
-  return a + b;
-}
-
-const result = add("string", "string2");
-
 /*
-type UnknownEmployee = Employee | Admin;
+const names: Array<string> = ["Max", "Manuel"];
 
-function printEmployeeInformation(emp: UnknownEmployee) {
-  console.log("Name: " + emp.name);
-  if ("privileges" in emp) console.log("Privileges: " + emp.privileges);
-  if ("startDate" in emp) console.log("Start Date: " + emp.startDate);
-}
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('This is done!')
+  }, 2000)
+})
 
-printEmployeeInformation(e1);
-
-class Car {}
-
-interface Bird {
-  type: "bird";
-  flyingSpeed: number;
-}
-
-interface Horse {
-  type: "horse";
-  runningSpeed: number;
-}
-
-type Animal = Bird | Horse;
-
-function moveAnimal(animal: Animal) {
-  let speed;
-  switch (animal.type) {
-    case "bird":
-      speed = animal.flyingSpeed;
-      break;
-    case "horse":
-      speed = animal.runningSpeed;
-      break;
-
-    default:
-      break;
-  }
-  console.log("Moving animal at " + speed);
-}
-moveAnimal({ type: "bird", flyingSpeed: 19 });
-
-// typecasting
-
-const para = document.querySelector("#message-output");
-// const userInputElement = <HTMLInputElement>document.querySelector("#user-input")!;
-const userInputElement = document.querySelector(
-  "#user-input"
-)! as HTMLInputElement;
-userInputElement.value = "Hi there!";
-
-// index types
-interface ErrorContainer {
-  [prop: string]: string;
-}
-
-const errorBag: ErrorContainer = {
-  email: "Not a valid email!",
-  username: "Must start with a capital letter!",
-};
+promise.then(data => {
+  data.split(' ');
+})
 */
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObjects = merge({ firstName: "Ian" }, { age: 33 });
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length > 0)
+    descriptionText = `Got ${element.length} element${
+      element.length === 1 ? "" : "s"
+    }.`;
+  return [element, descriptionText];
+}
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return obj[key];
+}
+
+extractAndConvert({ thing1: "thing", e: "234324" }, "e");
